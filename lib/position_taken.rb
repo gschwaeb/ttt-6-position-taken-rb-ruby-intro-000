5199ea6d57090dd5d957bd4e3f79a78b7bcdba3e
Gary Schwaeber <gschwaeb@gmail.com>
2019-10-14 23:48:28 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,4 @@
 def position_taken? (board, index)
-  board[index] == " " || board[index] == "" || board[index] == nil
+if board[index] == " " || board[index] == "" || board[index] == nil
+  return true
 end
