4bf4882d505bd599c648bb5290b11b8caf556143
Gary Schwaeber <gschwaeb@gmail.com>
2019-10-15 00:11:48 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 def position_taken? (board, index)
 if (board[index] ==  " " || board[index] == "" || board[index] == nil)
   return false
-elsif (board[index] ==  ("X"||"O") || board[index] == ("X"||"O") || board[index] == ("X"||"O"))
+elseif (board[index] ==  ("X"||"O")
   return true
 end
